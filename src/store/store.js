96d3b96ec3979e49e50d111c5c94e1bad5881868
Katherine <katherine.provostelara@gmail.com>
2022-10-10 16:54:56 -0300

    CalendarApp Frontend + Service Worker

--- src/store/store.js
+++ src/store/store.js
@@ -7,10 +7,10 @@ import { rootReducer } from '../reducers/rootReducer';
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 export const store = createStore(
-  rootReducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware( thunk )
+    )
 );
 
 
